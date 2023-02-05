0c9c492f1ce8d31956be4c4b3433b55c632a5009
Sockzzz <86269797+Sockzzz@users.noreply.github.com>
2023-02-05 11:42:12 -0800

    Initial Commit

---
+++ Pantry Portal/Pantry_PortalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pantry_PortalApp.swift
+//  Pantry Portal
+//
+//  Created by Marshall Jones on 2/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Pantry_PortalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
