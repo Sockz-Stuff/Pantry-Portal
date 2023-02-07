dd2db217315d070ab060f18d5c69d6ec484aeca8
Sockzzz <86269797+Sockzzz@users.noreply.github.com>
2023-02-06 16:50:18 -0800

    model files

--- Pantry Portal/PageTitleView.swift
+++ Pantry Portal/PageTitleView.swift
@@ -9,13 +9,24 @@ import SwiftUI
 
 struct PageTitleView: View {
     var title:String
+    var isDisplayingOrder:Bool! = nil
     var body: some View {
         HStack {
+            Spacer()
             Text(title)
                 .font(.largeTitle)
-            .fontWeight(.light)
-        }
-        
+                .fontWeight(.heavy)
+                .padding(.trailing)
+
+        }.overlay(
+            Image(systemName:isDisplayingOrder ?? false ? "chevron.up.square" : "chevron.down.square")
+                .font(.title)
+                .foregroundColor(isDisplayingOrder != nil ? Color("G1") : .clear)
+            .padding()
+            ,alignment: .leading
+        )
+        .foregroundColor(Color("G1"))
+        .background(Color("G4"))
     }
 }
 
